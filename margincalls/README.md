8d3b55a8425e6e77588dbd534e4712a35f95963f
Mikael Berger <mikaelberger@hotmail.es>
2019-10-18 10:00:24 +0200

    Initial commit for margincalls

---
+++ margincalls/README.md
@@ -0,0 +1,3 @@
+Compile and run the three programs in alphabetical order with sbt.
+
+Output is written to the ./data directory.
---
+++ margincalls/src/main/scala/utils/SimpleLog.scala
@@ -0,0 +1,42 @@
+package com.gimledigital.margincalls
+
+import scala.collection.mutable.ArrayBuffer
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+/** Provides simple log utility that registers the execution times for the steps
+ *  of an application and print them in console.
+ *
+ *  @param appName the name of the application
+ */
+class SimpleLog(appName: String) {
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+  val logBuffer = new ArrayBuffer[String]
+
+  // Prints the time when the application begins
+  def beginProgram = {
+    val date = dateFormat.format(new Date)
+
+    println("-"*80)
+    println(s"$date BEGIN PROGRAM $appName")
+    println("-"*80)
+  }
+
+ // Prints the time when an application step begins
+  def writeMsg(msg: String) = {
+    val date = dateFormat.format(new Date)
+
+    println(s"$date ${appName}: $msg")
+  }
+
+  // Prints the time when the application ends
+  def endProgram = {
+    val date = dateFormat.format(new Date)
+
+    println("-"*80)
+    println(s"$date END OF PROGRAM $appName")
+    println("-"*80)
+  }
+}
